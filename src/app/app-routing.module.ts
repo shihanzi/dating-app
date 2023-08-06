be63d4153cad9790369a51befccc40f55693f351
Shihan Mohamed <shihanmails@gmail.com>
2023-08-06 18:04:23 +0530

    front-end-changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,12 +9,18 @@ import { authGuard } from './_guard/auth.guard';
 
 
 const routes: Routes = [
-  {path:'',component:HomeComponent,},
-  {path:'members',component:MemberListComponent,canActivate:[authGuard]},
-  {path:'members:/id',component:MemberDetailComponent},
-  {path:'lists',component:ListComponent},
-  {path:'messages',component:MessagesComponent},
-  {path:'**',component:HomeComponent,pathMatch:'full'}
+  { path: '', component: HomeComponent, },
+  {
+    path: '',runGuardsAndResolvers:'always',
+    canActivate:[authGuard],
+    children:[
+      { path: 'members', component: MemberListComponent },
+      { path: 'members:/id', component: MemberDetailComponent },
+      { path: 'lists', component: ListComponent },
+      { path: 'messages', component: MessagesComponent }
+    ]
+  },
+  { path: '**', component: HomeComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
